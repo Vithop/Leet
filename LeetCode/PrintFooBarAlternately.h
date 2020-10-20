b11a8d7df6802c3811371d99292fa9a46bc9dcac
Vithop <vithuransoccer1@gmail.com>
2020-10-19 20:35:50 -0400

    Finished Q

--- LeetCode/PrintFooBarAlternately.h
+++ LeetCode/PrintFooBarAlternately.h
@@ -1,34 +1,48 @@
 class FooBar {
 private:
-    int n;
+    const int num;
+    int idx{0};
     mutex mtx;
-    condition_variable cvFoo, cvBar;
+    condition_variable cv;
 public:
-    FooBar(int n) {
-        this->n = n;
+    FooBar(int n): num(n*2){
+        cout<<(idx&0)<<endl;
     }
 
     void foo(function<void()> printFoo) {
 
-        unique_lock<mutex> lck(mtx);
-        for (int i = 0; i < n; i++) {
-            mtx.try_lock();
-        	// printFoo() outputs "foo". Do not change or remove this line.
-        	printFoo();
-            cvBar.notify_all();
-            cvFoo.wait(lck);
+        for (int i = 0; i < num; i++) {
+            unique_lock lck(mtx);
+            cv.wait(lck, [this](){
+                return idx >= num || idx % 2 == 0;
+            });
+            if(idx >= num){
+                break;
+            }
+            
+            // printFoo() outputs "foo". Do not change or remove this line.
+            printFoo();
+            ++idx;
+            lck.unlock();
+            cv.notify_all();
             
         }
     }
 
     void bar(function<void()> printBar) {
-        unique_lock<mutex> lck(mtx);
-        for (int i = 0; i < n; i++) {
-            cvBar.wait(lck);
-        	// printBar() outputs "bar". Do not change or remove this line.
-        	printBar();
-            cvFoo.notify_all();
-            mtx.unlock();
+        for (int i = 0; i < num; i++) {
+            unique_lock lck(mtx);
+            cv.wait(lck, [this](){
+                return idx >= num || idx % 2 == 1;
+            });
+            if(idx >= num){
+                break;
+            }
+            // printBar() outputs "bar". Do not change or remove this line.
+            printBar();
+            ++idx;
+            lck.unlock();
+            cv.notify_all();
             
         }
     }
